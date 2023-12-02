d7f1316ee5b73c510232bdf256e559c4281524d4
Айрат <ayratnasretdinov@gmail.com>
2023-12-02 12:03:48 +0300

    Приложение Pizza Recipes. Часть 3

--- app/src/main/java/com/example/pizzarecipes/MainActivity.java
+++ app/src/main/java/com/example/pizzarecipes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pizzarecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -14,11 +15,23 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView;
         RecyclerView.Adapter adapter;
         RecyclerView.LayoutManager layoutManager;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ArrayList<PizzaRecipeItem> pizzaRecipeItems = new ArrayList<>();
 
         pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.piz1,Utils.PIZZA_1_TITLE,Utils.PIZZA_1_DESCRIPTION, Utils.PIZZA_1_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.piz2,Utils.PIZZA_2_TITLE,Utils.PIZZA_2_DESCRIPTION, Utils.PIZZA_2_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.piz3,Utils.PIZZA_3_TITLE,Utils.PIZZA_3_DESCRIPTION, Utils.PIZZA_3_RECIPE));
+        pizzaRecipeItems.add(new PizzaRecipeItem(R.drawable.piz4,Utils.PIZZA_4_TITLE,Utils.PIZZA_4_DESCRIPTION, Utils.PIZZA_4_RECIPE));
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        adapter = new PizzaReceipeAdapter(pizzaRecipeItems);
+        layoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
--- app/src/main/java/com/example/pizzarecipes/PizzaReceipeAdapter.java
+++ app/src/main/java/com/example/pizzarecipes/PizzaReceipeAdapter.java
@@ -9,32 +9,51 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class PizzaReceipeAdapter extends RecyclerView.Adapter {
+import java.util.ArrayList;
+
+public class PizzaReceipeAdapter extends RecyclerView.Adapter<PizzaReceipeAdapter.PizzaReceipeViewHolder>  {
+    ArrayList<PizzaRecipeItem> pizzaRecipeItems;
+
+    public PizzaReceipeAdapter(ArrayList<PizzaRecipeItem> pizzaRecipeItems) {
+        this.pizzaRecipeItems = pizzaRecipeItems;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.pizza_recipe_item, viewGroup,false);
-        PizzaRecipeViewHolder pizzaRecipeViewHolder = new PizzaRecipeViewHolder(view);
+    public PizzaReceipeAdapter.PizzaReceipeViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.pizza_recipe_item, viewGroup, false);
+        PizzaReceipeViewHolder pizzaRecipeViewHolder = new PizzaReceipeViewHolder(view);
         return pizzaRecipeViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull PizzaReceipeViewHolder  holder, int position) {
+        PizzaRecipeItem pizzaRecipeItem = pizzaRecipeItems.get(position);
 
+        holder.pizzaImageView.setImageResource(pizzaRecipeItem.getImageResource());
+        holder.title.setText(pizzaRecipeItem.getTitle());
+        holder.description.setText(pizzaRecipeItem.getDescription());
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return pizzaRecipeItems.size();
     }
 
-    public static class PizzaRecipeViewHolder extends RecyclerView.ViewHolder{
+    public static class PizzaReceipeViewHolder extends RecyclerView.ViewHolder {
+
         public ImageView pizzaImageView;
         public TextView title;
         public TextView description;
 
-        public PizzaRecipeViewHolder(@NonNull View itemView) {
+        public PizzaReceipeViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            pizzaImageView = itemView.findViewById(R.id.pizzaImageView);
+            title = itemView.findViewById(R.id.titleImageView);
+            description = itemView.findViewById(R.id.descriptionImageView);
         }
+
     }
 }
